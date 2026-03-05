ab7b110d3f36df05eef07540a9a33a73211d55fc
AlexeyDemidow <usernzt@gmail.com>
2026-03-05 20:55:35 +0100

    Added llm_service.py for using llm

---
+++ llm_service.py
@@ -0,0 +1,24 @@
+import os
+import aiohttp
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = "https://router.huggingface.co/v1/chat/completions"
+
+headers = {
+    "Authorization": f"Bearer {os.getenv('HF_TOKEN')}",
+}
+
+async def ask_bot(messages: list):
+
+    payload = {
+        "model": "Qwen/Qwen3.5-35B-A3B:novita",
+        "messages": messages
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(API_URL, headers=headers, json=payload) as response:
+            result = await response.json()
+
+    return result["choices"][0]["message"]["content"]
