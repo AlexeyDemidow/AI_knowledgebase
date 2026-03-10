0e546e90cc88c0ef21ba826e4ac50c5732d38ed5
AlexeyDemidow <usernzt@gmail.com>
2026-03-10 19:27:51 +0100

    Fixed on start db conflict

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ load_dotenv(dotenv_path)
 app = FastAPI()
 
 
-@app.on_event("startup")
-async def on_startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+# @app.on_event("startup")
+# async def on_startup():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
 
 
 @app.post(
